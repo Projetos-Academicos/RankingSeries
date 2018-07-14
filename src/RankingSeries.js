7cf3fece79c21577625a9ba7b2cb832b88862864
Ricardo Lima <ricardoccofg@gmail.com>
2018-07-14 18:47:19 -0300

    iniciando com o redux

---
+++ src/RankingSeries.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, View} from 'react-native';
+import Navegacao from './Navegacao';
+
+const RankingSeries = props => (
+    <Navegacao />
+);
+
+
+const styles = StyleSheet.create({
+
+});
+export default RankingSeries;
